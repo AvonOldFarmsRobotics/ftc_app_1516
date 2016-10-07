2db6f701e6807b528346a9871e67a13113c010b0
DEDZTBH <peiqial@outlook.com>
2016-10-07 11:02:05 -0400

    alpha ver directive done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneJoystickPolarDirective.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneJoystickPolarDirective.java
@@ -28,7 +28,7 @@ public class OneJoystickPolarDirective extends AbstractDirective {
     @Inject.GamePad1(Inject.GamePadComponent.B)
     boolean b;
 
-    double power, angle, targetL, targetR;
+    static double power, angle, targetL, targetR;
 
     @Override
     public void onStart() {
@@ -46,53 +46,90 @@ public class OneJoystickPolarDirective extends AbstractDirective {
         power = Math.sqrt(X * X + Y * Y);
         if (X == 0f){
             if (-Y >= 0f){
-                angle = Math.PI/2;
+                //angle = Math.PI/2;
+                targetL = 1;
+                targetR = 1;
             }else{
-                angle = Math.PI*1.5;
+                //angle = Math.PI*1.5;
+                targetL = -1;
+                targetR = -1;
             }
         }else if(Y == 0f){
             if (X >= 0f){
-                angle = 0d;
+                //angle = 0d;
+                targetL = 1;
+                targetR = -1;
             }else {
-                angle = Math.PI;
+                //angle = Math.PI;
+                targetL = -1;
+                targetR = 1;
             }
         }else {
             angle = Math.atan(-Y / X);//since Y is reverse
             if (angle >= 0d){
                 if (X < 0f){
-                    angle += Math.PI;
+                    //angle += Math.PI;
+                    setMotorTarget(angle,3);
+                }else {
+                    setMotorTarget(angle,1);
                 }
             }else {
                 if (X < 0f){
-                    angle += Math.PI/2;
+                    //angle += Math.PI/2;
+                    setMotorTarget(angle,2);
                 }else {
-                    angle += Math.PI*1.5;
+                    //angle += Math.PI*1.5;
+                    setMotorTarget(angle,4);
                 }
             }
         }
 
-
-        if (angle >= Math.PI*1.5){
-        //[270,360)
-
-        }else if (angle >= Math.PI){
-        //[180,270)
-
-
-        }else if (angle >= Math.PI/2){
-        //[90,180)
-            targetL = 1 - (angle - Math.PI/2) / (Math.PI/2);
-            targetR = 1;
-        }else {
-        //[0,90)
-            targetL = 1;
-            targetR = angle / (Math.PI/2);
-        }
+//        if (angle >= Math.PI*1.5){
+//        //[270,360)
+//
+//        }else if (angle >= Math.PI){
+//        //[180,270)
+//
+//
+//        }else if (angle >= Math.PI/2){
+//        //[90,180)
+//            targetL = 1 - (angle - Math.PI/2) / (Math.PI/2);
+//            targetR = 1;
+//        }else {
+//        //[0,90)
+//            targetL = 1;
+//            targetR = angle / (Math.PI/2);
+//        }
 
         //}
+        dcMotorL.setPower(targetL*power);
+        dcMotorR.setPower(targetR*power);
 
         telemetry(X);
         telemetry(Y);
 
     }
+
+    void setMotorTarget(double angle, int quarter){
+        switch (quarter){
+            case 1:
+                targetL = 1;
+                targetR = angle / (Math.PI/2);
+                break;
+            case 2:
+                targetL = 1 - (-angle / (Math.PI/2));
+                targetR = 1;
+                break;
+            case 3:
+                targetL =  -angle / (Math.PI/2);
+                targetR = -1;
+                break;
+            case 4:
+                targetL = -1;
+                targetR = - (1 - (-angle / (Math.PI/2)));
+                break;
+        }
+    }
+
+
 }
