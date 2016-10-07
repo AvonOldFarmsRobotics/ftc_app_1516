63cefa0099a41ed1cd786d7df191971333323f16
DEDZTBH <peiqial@outlook.com>
2016-10-06 22:52:01 -0400

    atan fix in progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneJoystickPolarDirective.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneJoystickPolarDirective.java
@@ -28,12 +28,12 @@ public class OneJoystickPolarDirective extends AbstractDirective {
     @Inject.GamePad1(Inject.GamePadComponent.B)
     boolean b;
 
-    double power, angle;
+    double power, angle, targetL, targetR;
 
     @Override
     public void onStart() {
-        dcMotorL.setDirection(DcMotorSimple.Direction.FORWARD);
-        dcMotorR.setDirection(DcMotorSimple.Direction.REVERSE);
+        dcMotorL.setDirection(DcMotorSimple.Direction.REVERSE);
+        dcMotorR.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     @Override
@@ -44,13 +44,55 @@ public class OneJoystickPolarDirective extends AbstractDirective {
 
         //if (-Y > 0.1f || X > 0.1f) {
         power = Math.sqrt(X * X + Y * Y);
-        angle = Math.atan(-Y / X);//since Y is reverse
+        if (X == 0f){
+            if (-Y >= 0f){
+                angle = Math.PI/2;
+            }else{
+                angle = Math.PI*1.5;
+            }
+        }else if(Y == 0f){
+            if (X >= 0f){
+                angle = 0d;
+            }else {
+                angle = Math.PI;
+            }
+        }else {
+            angle = Math.atan(-Y / X);//since Y is reverse
+            if (angle >= 0d){
+                if (X < 0f){
+                    angle += Math.PI;
+                }
+            }else {
+                if (X < 0f){
+                    angle += Math.PI/2;
+                }else {
+                    angle += Math.PI*1.5;
+                }
+            }
+        }
 
 
+        if (angle >= Math.PI*1.5){
+        //[270,360)
+
+        }else if (angle >= Math.PI){
+        //[180,270)
+
+
+        }else if (angle >= Math.PI/2){
+        //[90,180)
+            targetL = 1 - (angle - Math.PI/2) / (Math.PI/2);
+            targetR = 1;
+        }else {
+        //[0,90)
+            targetL = 1;
+            targetR = angle / (Math.PI/2);
+        }
 
         //}
 
-        telemetry(Y);
         telemetry(X);
+        telemetry(Y);
+
     }
 }
